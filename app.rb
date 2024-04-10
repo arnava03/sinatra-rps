f2bf0d2a376e76ce9d10bb345357b4c0b95da0df
arnava03 <128776886+arnava03@users.noreply.github.com>
2024-04-10 00:35:21 +0000

    first attempt

--- app.rb
+++ app.rb
@@ -1,9 +1,53 @@
 require "sinatra"
 require "sinatra/reloader"
+require 'http'
+
+
+moves = ["rock", "paper", "scissors"]
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  wikipedia_url = "https://en.wikipedia.org/wiki/Rock_paper_scissors"
+  response = HTTP.get(wikipedia_url)
+  @html_content = response.body
+  
+  erb(:rules, { :layout => :layout })
+end
+
+get("/rock") do
+  @move = moves.sample
+  if @move == "rock"
+    @outcome = "tied"
+  elsif @move == "paper"
+    @outcome = "won"
+  else
+    @outcome = "lost"
+  end
+
+  erb(:rock, { :layout => :layout })
+end
+
+get("/paper") do
+  @move = moves.sample
+  if @move == "paper"
+    @outcome = "tied"
+  elsif @move == "rock"
+    @outcome = "won"
+  else
+    @outcome = "lost"
+  end
+
+  erb(:paper, { :layout => :layout })
+end
+
+get("/scissors") do
+  @move = moves.sample
+  if @move == "scissors"
+    @outcome = "tied"
+  elsif @move == "paper"
+    @outcome = "won"
+  else
+    @outcome = "lost"
+  end
+
+  erb(:scissors, { :layout => :layout })
 end
